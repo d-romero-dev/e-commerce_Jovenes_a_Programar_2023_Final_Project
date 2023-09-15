fbf9ff9159a646a5265f900334496ae1ab7130f3
Franpa99 <franparietti@gmail.com>
2023-09-15 00:08:20 -0300

    Mostrar comentarios

--- js/product-info.js
+++ js/product-info.js
@@ -1,4 +1,32 @@
+// Función para obtener comentarios
+function getComentarios(producto) {
+    let id = producto.id;
+    let comentariosEndpoint = `https://japceibal.github.io/emercado-api/products_comments/${id}.json`;
+    fetch(comentariosEndpoint)
+        .then((response) => response.json())
+        .then((comentarios) => {
+            mostrarComentarios(comentarios);
+        })
+        .catch((error) => {
+            console.error("Error: ", error);
+        });
+}
 
+// Función para mostrar comentarios
+function mostrarComentarios(comentarios) {
+    const comentariosContainer = document.getElementById("comentarios-container");
+    comentariosContainer.innerHTML = "";
+    comentarios.forEach((comentario) => {
+        const comentarioElement = document.createElement("div");
+        comentarioElement.innerHTML = `
+            <p>Usuario: ${comentario.user}</p>
+            <p>Puntuación: ${comentario.score}</p>
+            <p>Descripción: ${comentario.description}</p>
+            <p>Fecha y hora: ${comentario.dateTime}</p>
+        `;
+        comentariosContainer.appendChild(comentarioElement);
+    });
+}
 
 //Función encargada de Mostrar en pantalla la calificación generada en Comentarios.
 document.addEventListener("DOMContentLoaded", function () {
