ab99aa88df373b09ee344c9be507eef4cf4de514
florencia190 <florenciapricoli14@gmail.com>
2023-09-30 00:15:16 -0300

    Alineacion dropdown

--- js/init.js
+++ js/init.js
@@ -7,36 +7,36 @@ const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
 const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
 const EXT_TYPE = ".json";
 
-let showSpinner = function(){
+let showSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "block";
 }
 
-let hideSpinner = function(){
+let hideSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "none";
 }
 
-let getJSONData = function(url){
-    let result = {};
-    showSpinner();
-    return fetch(url)
+let getJSONData = function (url) {
+  let result = {};
+  showSpinner();
+  return fetch(url)
     .then(response => {
       if (response.ok) {
         return response.json();
-      }else{
+      } else {
         throw Error(response.statusText);
       }
     })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          hideSpinner();
-          return result;
+    .then(function (response) {
+      result.status = 'ok';
+      result.data = response;
+      hideSpinner();
+      return result;
     })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-        hideSpinner();
-        return result;
+    .catch(function (error) {
+      result.status = 'error';
+      result.data = error;
+      hideSpinner();
+      return result;
     });
 }
 /**
@@ -48,38 +48,49 @@ function userIsLogged() {
   return localStorage.getItem('userIsLogged') === "true";
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  if (!userIsLogged()) {
-    window.location.href = "login.html";
-  }
-  botonCerrarSesion= document.getElementById("cerrarSesion")
-  botonCerrarSesion.addEventListener("click", () => {
-    setUserIsLogged(false);
-    window.location.href = "login.html";
-  } )
-}); 
-
 function setUserIsLogged(isLogged) {
   // Doble negación para evita asignar algo que no sea Bool
   localStorage.setItem('userIsLogged', !!isLogged);
 }
 
+//Genera un menu desplegable en el boton de nombre de usuario
+function generarMenuDesplegable() {
+  const username = localStorage.getItem("user");
+  let htmlContentToAppend = `
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+    `+
+    username[0].toUpperCase() + username.slice(1).toLowerCase() 
+    +
+    `
+    </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a class="dropdown-item" href="cart.html">Mi carrito</a></li>
+    <li><a class="dropdown-item" href="my-profile.html">Mi perfil</a></li>
+    <li><a class="dropdown-item" id="cierreDeSesion" >Cerrar sesión</a></li>
+  </ul>
+  `
+  
+  let barraNavegacion = document.querySelector("#navbarNav>ul:first-child");
+  let menuDesplegable = document.createElement("li");
+  menuDesplegable.className = "nav-item dropdown"; 
+  menuDesplegable.innerHTML = htmlContentToAppend;
+  barraNavegacion.appendChild(menuDesplegable)
 
+  botonCerrarSesion = document.getElementById("cierreDeSesion");
+  botonCerrarSesion.addEventListener("click", () => {
+    localStorage.removeItem("user");
+    setUserIsLogged(false);
+    window.location.href = "login.html";
+  })
+}
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
+  if (!userIsLogged()) {
+    window.location.href = "login.html";
+  }
   // Verifica si el usuario ya está logueado
   if (userIsLogged()) {
-    const username = localStorage.getItem("user");
-    // Si está logueado muestra el nombre guardado en el almacenamiento local 
-    document.getElementById("cerrarSesion").textContent = "Cerrar Sesión de " + username[0].toUpperCase() + username.slice(1).toLowerCase();
+    // Si está logueado muestra button, con el nombre guardado en el almacenamiento local y un dropdown
+    generarMenuDesplegable();
   }
-
-  document.getElementById('cerrarSesion').addEventListener('click', function () {
-    // Al hacer clic en "Cerrar Sesión", elimina el nombre de usuario del almacenamiento local
-    localStorage.removeItem("user");
-    // Redirige a la página de inicio de sesión o donde lo necesites
-    window.location.href = "login.html";
-  });
 });
-
-
