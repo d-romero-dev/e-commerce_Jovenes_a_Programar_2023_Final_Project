7de6421bc6c6550efa553eaa4bdf7d7f9f08c302
Heltta <lucashelgueraz@gmail.com>
2023-09-30 22:11:36 -0300

    Format init.js with Prettier

--- js/init.js
+++ js/init.js
@@ -1,25 +1,27 @@
-const CATEGORIES_URL = "https://japceibal.github.io/emercado-api/cats/cat.json";
-const PUBLISH_PRODUCT_URL = "https://japceibal.github.io/emercado-api/sell/publish.json";
-const PRODUCTS_URL = "https://japceibal.github.io/emercado-api/cats_products/";
-const PRODUCT_INFO_URL = "https://japceibal.github.io/emercado-api/products/";
-const PRODUCT_INFO_COMMENTS_URL = "https://japceibal.github.io/emercado-api/products_comments/";
-const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
-const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
-const EXT_TYPE = ".json";
+const CATEGORIES_URL = 'https://japceibal.github.io/emercado-api/cats/cat.json';
+const PUBLISH_PRODUCT_URL =
+  'https://japceibal.github.io/emercado-api/sell/publish.json';
+const PRODUCTS_URL = 'https://japceibal.github.io/emercado-api/cats_products/';
+const PRODUCT_INFO_URL = 'https://japceibal.github.io/emercado-api/products/';
+const PRODUCT_INFO_COMMENTS_URL =
+  'https://japceibal.github.io/emercado-api/products_comments/';
+const CART_INFO_URL = 'https://japceibal.github.io/emercado-api/user_cart/';
+const CART_BUY_URL = 'https://japceibal.github.io/emercado-api/cart/buy.json';
+const EXT_TYPE = '.json';
 
 let showSpinner = function () {
-  document.getElementById("spinner-wrapper").style.display = "block";
-}
+  document.getElementById('spinner-wrapper').style.display = 'block';
+};
 
 let hideSpinner = function () {
-  document.getElementById("spinner-wrapper").style.display = "none";
-}
+  document.getElementById('spinner-wrapper').style.display = 'none';
+};
 
 let getJSONData = function (url) {
   let result = {};
   showSpinner();
   return fetch(url)
-    .then(response => {
+    .then((response) => {
       if (response.ok) {
         return response.json();
       } else {
@@ -38,14 +40,14 @@ let getJSONData = function (url) {
       hideSpinner();
       return result;
     });
-}
+};
 /**
  * Obtener estado de logged del usuario
  *
  * Obtiene del localStorage el valor de la data de key 'userIsLogged' en el localStorage
  */
 function userIsLogged() {
-  return localStorage.getItem('userIsLogged') === "true";
+  return localStorage.getItem('userIsLogged') === 'true';
 }
 
 function setUserIsLogged(isLogged) {
@@ -55,12 +57,13 @@ function setUserIsLogged(isLogged) {
 
 //Genera un menu desplegable en el boton de nombre de usuario
 function generarMenuDesplegable() {
-  const username = localStorage.getItem("user");
-  let htmlContentToAppend = `
+  const username = localStorage.getItem('user');
+  let htmlContentToAppend =
+    `
     <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-    `+
-    username[0].toUpperCase() + username.slice(1).toLowerCase() 
-    +
+    ` +
+    username[0].toUpperCase() +
+    username.slice(1).toLowerCase() +
     `
     </button>
   <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
@@ -68,25 +71,25 @@ function generarMenuDesplegable() {
     <li><a class="dropdown-item" href="my-profile.html">Mi perfil</a></li>
     <li><a class="dropdown-item" id="cierreDeSesion" >Cerrar sesión</a></li>
   </ul>
-  `
-  
-  let barraNavegacion = document.querySelector("#navbarNav>ul:first-child");
-  let menuDesplegable = document.createElement("li");
-  menuDesplegable.className = "nav-item dropdown"; 
-  menuDesplegable.innerHTML = htmlContentToAppend;
-  barraNavegacion.appendChild(menuDesplegable)
+  `;
 
-  botonCerrarSesion = document.getElementById("cierreDeSesion");
-  botonCerrarSesion.addEventListener("click", () => {
-    localStorage.removeItem("user");
+  let barraNavegacion = document.querySelector('#navbarNav>ul:first-child');
+  let menuDesplegable = document.createElement('li');
+  menuDesplegable.className = 'nav-item dropdown';
+  menuDesplegable.innerHTML = htmlContentToAppend;
+  barraNavegacion.appendChild(menuDesplegable);
+
+  botonCerrarSesion = document.getElementById('cierreDeSesion');
+  botonCerrarSesion.addEventListener('click', () => {
+    localStorage.removeItem('user');
     setUserIsLogged(false);
-    window.location.href = "login.html";
-  })
+    window.location.href = 'login.html';
+  });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
   if (!userIsLogged()) {
-    window.location.href = "login.html";
+    window.location.href = 'login.html';
   }
   // Verifica si el usuario ya está logueado
   if (userIsLogged()) {
