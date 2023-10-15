c5db550f6b3595f909baea729693a0bebe790ed6
rodriguez13-byte <leguizamon3718@gmail.com>
2023-10-15 11:36:00 -0300

    cambios carro

--- js/cart.js
+++ js/cart.js
@@ -9,21 +9,19 @@ function eliminarEnDesarrollo() {
 
 function getCarritodeCompras() {
     return fetch(APIcarrito)
-        .then(response => {if (response.ok){
-            return response.json();
-        }else{
-            throw Error(response.statusText);
-        }})
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw Error(response.statusText);
+            }
+        })
         .then(carrito => {
             return carrito;
         })
-        
         .catch((error) => {
             console.error("Error: ", error);
         });
-
-       
-
 }
 
 function showCarritodeCompras(carrito) {
@@ -46,17 +44,37 @@ function showCarritodeCompras(carrito) {
                 <td>
                 <img src="${carrito.articles[0].image}" width="100" alt= "Imagen del articulo">
                 </td>
-                <td >${carrito.articles[0].name}</td>
+                <td>${carrito.articles[0].name}</td>
                 <td>${carrito.articles[0].currency} ${carrito.articles[0].unitCost}</td>
-                <td> <input type="number" value="${carrito.articles[0].count}" /></td>
-                <td>${carrito.articles[0].currency} ${carrito.articles[0].unitCost * carrito.articles[0].count}</td>
+                <td> <input type="number" id="cantidadInput" value="${carrito.articles[0].count}" /></td>
+                <td>${carrito.articles[0].currency} <span id="subtotal">${carrito.articles[0].unitCost * carrito.articles[0].count}</span></td>
             
             </tr>
         </table>
-
-    `
+    `;
     carritoContainer.innerHTML = htmlContentToAppend;
 
+    // Obtén los elementos necesarios del DOM
+    const cantidadInput = document.getElementById('cantidadInput');
+    const subtotalElement = document.getElementById('subtotal');
+
+    // Función para calcular y actualizar el subtotal
+    function calcularSubtotal() {
+        // Obtiene la cantidad del campo de entrada
+        const cantidad = parseInt(cantidadInput.value);
+
+        // Calcula el subtotal multiplicando el precio unitario por la cantidad
+        const subtotal = carrito.articles[0].unitCost * cantidad;
+
+        // Actualiza el contenido del elemento de subtotal en la página
+        subtotalElement.textContent = `${carrito.articles[0].currency} ${subtotal}`;
+    }
+
+    // Agrega un evento de entrada al campo de cantidad para llamar a la función calcularSubtotal() cuando cambie el valor
+    cantidadInput.addEventListener('input', calcularSubtotal);
+
+    // Calcula el subtotal inicial al cargar la página
+    calcularSubtotal();
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -64,7 +82,5 @@ document.addEventListener("DOMContentLoaded", function () {
     getCarritodeCompras()
     .then (function (carrito){
         showCarritodeCompras(carrito);
-    })
-    
+    });
 });
-
